491c73500fe2cadf75e1bac849524671e171ad04
gedu17 <dulskasg@gmail.com>
2023-12-10 12:15:42 +0200

    Day Three part 2

--- DayThree/DayThree.cs
+++ DayThree/DayThree.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using aoc2023;
 
@@ -8,66 +9,133 @@ public static class DayThree
     {
         var file = await File.ReadAllLinesAsync($"{nameof(DayThree)}/input.txt");
         var results = new List<int>();
-        var charList = new List<char>();
 
         for (var i = 0; i < file.Length; i++)
         {
             var previousLine = file.ElementAtOrDefault(i - 1);
             var currentLine = file.ElementAtOrDefault(i);
             var nextLine = file.ElementAtOrDefault(i + 1);
-            charList.Clear();
-            int? numberStartedAt = null;
 
             for (var j = 0; j < currentLine!.Length; j++)
             {
-                if ((int)currentLine[j] is >= 48 and <= 57)
-                {
-                    charList.Add(currentLine[j]);
-
-                    numberStartedAt ??= j;
-
-                    if (currentLine.ElementAtOrDefault(j + 1) != default)
-                    {
-                        continue;
-                    }
-                }
-
-                if (charList.Count == 0) 
+                if (currentLine[j] != '*')
                 {
                     continue;
                 }
 
-                var symbolsAround = new List<char>();
+                var numbers = new List<int?>();
 
-                // symbolOnTheLeft
-                if (numberStartedAt!.Value > 0)
+                if (char.IsDigit(currentLine.ElementAtOrDefault(j - 1)))
                 {
-                    symbolsAround.Add(currentLine.ElementAtOrDefault(numberStartedAt!.Value - 1));
+                    numbers.Add(ReadNumberRtl(currentLine, j - 1));
                 }
 
-                // symbolOnTheRight
-                if (currentLine.ElementAtOrDefault(j + 1) != default)
+                if (char.IsDigit(currentLine.ElementAtOrDefault(j + 1)))
                 {
-                    symbolsAround.Add(currentLine.ElementAtOrDefault(j));
+                    numbers.Add(ReadNumberLtr(currentLine, j + 1));
                 }
 
-                // symbolsAbove
-                symbolsAround.AddRange(previousLine?[(numberStartedAt!.Value > 0 ? numberStartedAt!.Value - 1 : 0)..(j + 1)]?.ToCharArray() ?? []);
-                // symbolsBelow
-                symbolsAround.AddRange(nextLine?[(numberStartedAt!.Value > 0 ? numberStartedAt!.Value - 1 : 0)..(j + 1)]?.ToCharArray() ?? []);
-                symbolsAround.RemoveAll(x => x == '.');
-
-                if (symbolsAround.Any(x => char.IsAsciiDigit(x) is false))
+                if (previousLine is not null)
                 {
-                    results.Add(int.Parse(charList.ToArray()));
+                    numbers.AddRange(GetNumbers(previousLine!, j));
                 }
 
-                charList.Clear();
-                numberStartedAt = null;
+                if (nextLine is not null)
+                {
+                    numbers.AddRange(GetNumbers(nextLine!, j));
+                }
+
+                numbers = numbers.Where(x => x.HasValue).ToList();
+                
+                if (numbers.Count == 2)
+                {          
+                    results.Add(numbers.First()!.Value * numbers.Skip(1).First()!.Value);
+                }
             }
         }
 
         System.Console.WriteLine($"{nameof(DayThree)} result: {results.Sum()}");
+    }
+
+    public static List<int?> GetNumbers(string line, int gearPosition)
+    {
+        var beforeGear = line.ElementAtOrDefault(gearPosition - 1);
+        var atGear = line.ElementAtOrDefault(gearPosition);
+        var afterGear = line.ElementAtOrDefault(gearPosition + 1);
+        var result = new List<int?>();
+
+        if (char.IsDigit(beforeGear) && char.IsDigit(atGear) && char.IsDigit(afterGear))
+        {
+            result.Add(ReadNumberLtr(line, gearPosition - 1));
+        }
+
+        if (char.IsDigit(beforeGear) && char.IsDigit(atGear) is false && char.IsDigit(afterGear) is false)
+        {
+            result.Add(ReadNumberRtl(line, gearPosition - 1));
+        }
+
+        if (char.IsDigit(beforeGear) && char.IsDigit(atGear) && char.IsDigit(afterGear) is false)
+        {
+            result.Add(ReadNumberRtl(line, gearPosition));
+        }
+
+        if (char.IsDigit(beforeGear) is false && char.IsDigit(atGear) && char.IsDigit(afterGear))
+        {
+            result.Add(ReadNumberLtr(line, gearPosition));
+        }
+
+        if (char.IsDigit(beforeGear) is false && char.IsDigit(atGear) is false && char.IsDigit(afterGear))
+        {
+            result.Add(ReadNumberLtr(line, gearPosition + 1));
+        }
+
+        if (char.IsDigit(beforeGear) && char.IsDigit(atGear) is false && char.IsDigit(afterGear))
+        {
+            result.Add(ReadNumberRtl(line, gearPosition - 1));
+            result.Add(ReadNumberLtr(line, gearPosition + 1));
+        }
+
+        return result;
+    }
+
+    private static int ReadNumberRtl(string line, int startingPosition)
+    {
+        var result = new List<char>();
+
+        for (var i = startingPosition; i >= 0; i--)
+        {
+            if (char.IsDigit(line[i]))
+            {
+                result.Add(line[i]);
+
+                continue;
+            }
+
+            break;
+        }
+
+        result.Reverse();
+
+        return int.Parse(result.ToArray());
+    }
+
+    private static int ReadNumberLtr(string line, int startingPosition)
+    {
+        var result = new List<char>();
+
+        for (var i = startingPosition; i < line.Length; i++)
+        {
+            if (char.IsDigit(line[i]))
+            {
+                result.Add(line[i]);
+
+                continue;
+            }
+
+            break;
+        }
+
+        return int.Parse(result.ToArray());
     }
 }
 
