b7e86caa74fcee8430c32fa6c142cadcdaa7478a
gedu17 <dulskasg@gmail.com>
2023-12-10 13:12:51 +0200

    Day four part 2

--- DayFour/DayFour.cs
+++ DayFour/DayFour.cs
@@ -8,21 +8,75 @@ public static class DayFour
     public static async Task Solve()
     {
         var file = await File.ReadAllLinesAsync($"{nameof(DayFour)}/input.txt");
-        var results = new List<int>();
+        var result = 0;
+        var duplicates = new Dictionary<int, int>();
 
-        foreach (var line in file)
+        for (var i = 0; i < file.Length; i++)
         {
-            var cardSplit = line.Split(":");
-            var gameSplit = cardSplit[1].Split("|");
+            var matchCount = GetMatchCount(file[i]);
+            var filled = FillDuplicates(duplicates, i, file.Length, matchCount);
             
-            var winningNumbers = gameSplit[0].Trim().Split(" ").Where(x => x != string.Empty).Select(int.Parse);
-            var gameNumbers = gameSplit[1].Trim().Split(" ").Where(x => x != string.Empty).Select(int.Parse);
-
-            var result = gameNumbers.Intersect(winningNumbers).Aggregate(0, (x, y) => x == 0 ? 1 : x * 2);
-            
-            results.Add(result);
+            result += filled + 1;
         }
 
-        System.Console.WriteLine($"{nameof(DayFour)} result: {results.Sum()}");
+        do 
+        {
+            for (var i = 0; i <= file.Length; i++)
+            {
+                if (duplicates.TryGetValue(i, out var count) is false || count == 0)
+                {
+                    continue;
+                }
+
+                foreach (var j in Enumerable.Range(1, count))
+                {
+                    var matchCount = GetMatchCount(file[i]);
+                    var filled = FillDuplicates(duplicates, i, file.Length, matchCount);
+                
+                    result += filled;
+                }
+                
+                duplicates[i] = 0;
+            }
+        } while (duplicates.Sum(x => x.Value) > 0);
+
+
+        System.Console.WriteLine($"{nameof(DayFour)} result: {result}");
+    }
+
+    private static int GetMatchCount(string row)
+    {
+        var cardSplit = row.Split(":");
+        var gameSplit = cardSplit[1].Split("|");
+        
+        var winningNumbers = gameSplit[0].Trim().Split(" ").Where(x => x != string.Empty).Select(int.Parse);
+        var gameNumbers = gameSplit[1].Trim().Split(" ").Where(x => x != string.Empty).Select(int.Parse);
+
+        return gameNumbers.Intersect(winningNumbers).Count();
+    }
+
+    private static int FillDuplicates(Dictionary<int, int> duplicates, int currentLine, int lineCount, int matchCount)
+    {
+        var result = 0;
+
+        for (var j = 0; j < matchCount; j++)
+        {
+            var idx = currentLine + j + 1;
+            
+            if (idx > lineCount)
+            {
+                continue;
+            }
+
+            if (duplicates.ContainsKey(idx) is false)
+            {
+                duplicates[idx] = 0;
+            }
+
+            duplicates[idx]++;
+            result++;
+        }
+
+        return result;
     }
 }
